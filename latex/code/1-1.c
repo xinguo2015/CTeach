b81e943b16284e30310003a4334328a211da7148
Xinguo Liu <xgliu@cad.zju.edu.cn>
2016-09-07 13:06:09 +0800

    remove pdf, ignore temp fold, add proj.vim.

--- latex/code/1-1.c
+++ latex/code/1-1.c
@@ -1,14 +1,14 @@
-#include <stdio.h> 
-int main(void)
+#include <stdio.h>               /* 编译预处理命令 */
+int factorial(int n);            /* 函数声明 */
+int main(void)                   /* 主函数 */
 {
-	int n; 
-	int factorial(int n); 
-	scanf("%d", &n);
-	printf("%d\n", factorial(n));
+	int n;                       /* 定义变量 */
+	scanf("%d", &n);             /* 输入一个整数 */
+	printf("%d\n", factorial(n));/* 调用函数计算阶乘并输出 */
 	return 0;
 }
 
-int factorial(int n)
+int factorial(int n)             /* 定义计算n!的函数 */
 {
 	int i, fact = 1;
 	for(i = 1; i <= n; i++) 
